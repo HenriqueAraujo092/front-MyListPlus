d32e918a707d29599ad0eadeda25b36c528b9b14
HenriqueAraujo092 <henrique.araujo092@hotmail.com>
2021-10-27 21:50:11 -0300

    Adicionando so um botao de mais listas

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   exports: [
     MatCardModule,
     MatTableModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule
   ],
 })
 export class AppMaterialModule { }
